3ebc45169840d12827a4777e77eaa4662e41cf18
Calle <calle.tuvesson@oddhill.se>
2014-07-01 09:00:45 +0200

    Fix the js file + add modernizr script that replaces .svg with .png if not supported.

--- js/script.js
+++ js/script.js
@@ -1,12 +1,24 @@
-
-$(document).ready(function() {
-	$(".background").imgLiquid();
-
+// Function that centeres the info box.
+var centerBox = function() {
   var box = $('.info');
   var boxHeight = box.outerHeight(false);
 
-  box.css(
-    "margin-top", -boxHeight/2
-  );
+  box.css({"margin-top": 0 - boxHeight/2});
+};
+
+$(document).ready(function() {
+
+  // Fit the background image.
+	$(".background").imgLiquid();
+
+  // Center the info box.
+  centerBox();
+
+  // Replace svg images with png if the browser doesn't support svg.
+  if (!Modernizr.svg) {
+    $('img[src$=".svg"]').attr('src', function() {
+      return $(this).attr('src').replace('.svg', '.png');
+    });
+  }
 
 });
